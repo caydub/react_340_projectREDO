2cb946092e494ac8cb85e73f3c77a299f300b90b
Andy <walshrandrew@gmail.com>
2025-11-20 14:14:27 -0700

    updated GenericDeleteButton and GenericUpdateButton

--- frontend/src/components/GenericDeleteButton.jsx
+++ frontend/src/components/GenericDeleteButton.jsx
@@ -1,14 +1,38 @@
-const GenericDeleteButton = ({ rowObject, backendURL, refreshRows }) => {
+const GenericDeleteButton = ({ rowObject, backendURL, refreshRows, deleteIdField, deleteRoute }) => {
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const formData = {
+            [deleteIdField]: rowObject[deleteIdField],
+        };
+
+        try {
+            const response = await fetch(`${backendURL}${deleteRoute}`, {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(formData),
+            });
+
+            if (response.ok) {
+                console.log("Row deleted successfully.");
+                refreshRows();
+            } else {
+                console.error("Error deleting row.");
+            }
+        } catch (error) {
+            console.error('Error during deletion:', error);
+        }
+    };
 
     return (
         <td>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <button type='submit'>
                     Delete
                 </button>
             </form>
         </td>
-
     );
 };
 
